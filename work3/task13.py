fe73fe32a2cac99e4d80ece2765387a0446bdf43
krissstyu02 <kristi090217@gmail.com>
2026-03-03 17:42:34 +0300

    work 3 - task13

---
+++ work3/task13.py
@@ -0,0 +1,50 @@
+import requests
+import pytest
+
+
+class TestHomeworkUserAgent:
+
+    @pytest.mark.parametrize(
+        "user_agent, expected_platform, expected_browser, expected_device",
+        [
+            (
+                "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+                "Mobile", "No", "Android"
+            ),
+            (
+                "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+                "Mobile", "Chrome", "iOS"
+            ),
+            (
+                "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                "Googlebot", "Unknown", "Unknown"
+            ),
+            (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+                "Web", "Chrome", "No"
+            ),
+            (
+                "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+                "Mobile", "No", "iPhone"
+            ),
+        ]
+    )
+    def test_homework_user_agent(self, user_agent, expected_platform, expected_browser, expected_device):
+
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+
+        response = requests.get(
+            url,
+            headers={"User-Agent": user_agent}
+        )
+
+        response_json = response.json()
+
+        assert response_json["platform"] == expected_platform, \
+            f"Wrong platform for UA: {user_agent}"
+
+        assert response_json["browser"] == expected_browser, \
+            f"Wrong browser for UA: {user_agent}"
+
+        assert response_json["device"] == expected_device, \
+            f"Wrong device for UA: {user_agent}"
