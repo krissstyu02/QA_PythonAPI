073bac21f3181d5f281fbd0c6545fbe3a97175c5
krissstyu02 <kristi090217@gmail.com>
2026-03-31 17:17:30 +0300

    task16

--- work4.0/tests/test_user_edit.py
+++ work4.0/tests/test_user_edit.py
@@ -42,3 +42,130 @@ class TestUserEdit(BaseCase):
                                    headers={"x-csrf-token": token},
                                    cookies={"auth_sid": auth_sid})
         Assertions.assert_json_value_by_name(response4, "firstName", new_name, "Wrong name of user after edit")
+
+    def test_edit_not_auth_user(self):
+        new_name = 'Changed Name'
+
+        response3 = MyRequests.put(f"/user/1",
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_value_by_name(response3, "error", "Auth token not supplied", "Wrong error")
+
+
+    def test_edit_another_user(self):
+        # REGISTER
+        register_data = self.prepare_registartion_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # AUTHORIZATION
+        login_data = {
+            'email': email,
+            'password': password,
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        another_user_id = 16 if user_id != 16 else 17
+
+        # EDIT
+        new_name = 'Changed Name'
+
+        response3 = MyRequests.put(f"/user/{another_user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_has_key(response3, "error")
+
+
+    def test_edit_with_not_valid_email(self):
+        # REGISTER
+        register_data = self.prepare_registartion_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # AUTHORIZATION
+        login_data = {
+            'email': email,
+            'password': password,
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_email = 'email123.com'
+
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"email": new_email}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_value_by_name(response3, "error", "Invalid email format", "Wrong error")
+
+        # GET
+        response4 = MyRequests.get(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+        Assertions.assert_json_value_by_name(response4, "email", email, "New email of user after negative edit")
+
+
+    def test_edit_with_not_valid_name(self):
+        # REGISTER
+        register_data = self.prepare_registartion_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        first_name = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # AUTHORIZATION
+        login_data = {
+            'email': email,
+            'password': password,
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_name = 'e'
+
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_value_by_name(response3, "error", "The value for field `firstName` is too short", "Wrong error")
+
+        # GET
+        response4 = MyRequests.get(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+        Assertions.assert_json_value_by_name(response4, "firstName", first_name, "New name of user after negative edit")
